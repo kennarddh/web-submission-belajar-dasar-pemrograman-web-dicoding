98345911089bf616da0b22c046983bafdde0abe1
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-12 23:35:22 +0700

    feat: Resposive navbar

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,11 @@
 const toggleNav = document.querySelector('header .toggle-nav')
+const headerNav = document.querySelector('header nav')
 
 toggleNav.addEventListener('click', () => {
-    
 	toggleNav.classList.toggle('change')
+	headerNav.classList.toggle('showing')
+})
+
+window.addEventListener('load', () => {
+	document.body.classList.remove('preload')
 })
