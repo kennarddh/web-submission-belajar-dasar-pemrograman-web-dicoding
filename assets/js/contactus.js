62740e9686e341b5fda76ea988fd3df1ced9eed5
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-13 01:33:39 +0700

    feat: Send button contact us dropdown

--- assets/js/contactus.js
+++ assets/js/contactus.js
@@ -1,7 +1,8 @@
 const contactUsNameInput = document.querySelector('#contactus #name')
 const contactUsEmailInput = document.querySelector('#contactus #email')
 const contactUsMessageInput = document.querySelector('#contactus #message')
-const contactUsSubmitButton = document.querySelector('#contactus button')
+const contactUsSubmitButton = document.querySelector('#contactus button#submit')
+const contactUsSendButton = document.querySelector('#contactus button#send')
 
 const validateInput = () => {
 	if (
@@ -10,13 +11,51 @@ const validateInput = () => {
 		!contactUsMessageInput.validity.valid
 	) {
 		contactUsSubmitButton.disabled = true
+		contactUsSendButton.disabled = true
 
 		return
 	}
 
 	contactUsSubmitButton.disabled = false
+	contactUsSendButton.disabled = false
 }
 
 contactUsNameInput.addEventListener('input', validateInput)
 contactUsEmailInput.addEventListener('input', validateInput)
 contactUsMessageInput.addEventListener('input', validateInput)
+
+contactUsSubmitButton.addEventListener(
+	'mouseover',
+	() => {
+		contactUsSendButton.style.opacity = 1
+		contactUsSendButton.style.visibility = 'visible'
+	},
+	false
+)
+
+contactUsSubmitButton.addEventListener(
+	'mouseout',
+	() => {
+		contactUsSendButton.style.opacity = 0
+		contactUsSendButton.style.visibility = 'hidden'
+	},
+	false
+)
+
+contactUsSendButton.addEventListener(
+	'mouseover',
+	() => {
+		contactUsSendButton.style.opacity = 1
+		contactUsSendButton.style.visibility = 'visible'
+	},
+	false
+)
+
+contactUsSendButton.addEventListener(
+	'mouseout',
+	() => {
+		contactUsSendButton.style.opacity = 0
+		contactUsSendButton.style.visibility = 'hidden'
+	},
+	false
+)
